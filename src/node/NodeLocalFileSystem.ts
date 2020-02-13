22c0d8e3bf8a61a885924c077718a4fff431272f
Yoshihiro Seki <nikochan2k@gmail.com>
2020-02-13 23:38:33 +0900

    organize packages

--- src/node/NodeLocalFileSystem.ts
+++ src/node/NodeLocalFileSystem.ts
@@ -1,12 +1,18 @@
 import { AbstractAccessor, AbstractLocalFileSystem, Permission } from "kura";
 import { NodeAccessor } from "./NodeAccessor";
 
+const LAST_SLASH = /\/+$/.compile();
+
 export class NodeLocalFileSystem extends AbstractLocalFileSystem {
+  private rootDir: string;
+
   constructor(rootDir: string);
   constructor(rootDir: string, useIndex: boolean);
   constructor(rootDir: string, permission: Permission);
-  constructor(private rootDir: string, config?: any) {
+  constructor(rootDir: string, config?: any) {
     super(config);
+    rootDir = rootDir.replace(LAST_SLASH, "");
+    this.rootDir = rootDir;
   }
 
   protected createAccessor(): Promise<AbstractAccessor> {
