14f5c755329f9148b6d1a411652ac074bec6e3af
Yoshihiro Seki <nikochan2k@gmail.com>
2020-03-07 15:12:33 +0900

    use normalizePath

--- src/node/NodeLocalFileSystem.ts
+++ src/node/NodeLocalFileSystem.ts
@@ -1,7 +1,7 @@
 import {
   AbstractAccessor,
   AbstractLocalFileSystem,
-  LAST_DIR_SEPARATORS,
+  normalizePath,
   Permission
 } from "kura";
 import { NodeAccessor } from "./NodeAccessor";
@@ -14,8 +14,7 @@ export class NodeLocalFileSystem extends AbstractLocalFileSystem {
   constructor(rootDir: string, permission: Permission);
   constructor(rootDir: string, config?: any) {
     super(config);
-    rootDir = rootDir.replace(LAST_DIR_SEPARATORS, "");
-    this.rootDir = rootDir;
+    this.rootDir = normalizePath(rootDir);
   }
 
   protected createAccessor(): Promise<AbstractAccessor> {
