a98179007ab65cdb970f1671d2b0a829aeff94e6
Yoshihiro Seki <nikochan2k@gmail.com>
2020-03-14 14:34:00 +0900

    add polyfill for node

--- src/node/NodeLocalFileSystem.ts
+++ src/node/NodeLocalFileSystem.ts
@@ -2,6 +2,15 @@ import { AbstractAccessor, AbstractLocalFileSystem, normalizePath } from "kura";
 import { FileSystemOptions } from "kura/lib/FileSystemOptions";
 import { NodeAccessor } from "./NodeAccessor";
 
+if (
+  !(global && global.setTimeout && global.clearTimeout) &&
+  !(window && window.setTimeout && window.clearTimeout)
+) {
+  const timers = require("timers");
+  global.clearTimeout = timers.clearTimeout;
+  global.setTimeout = timers.setTimeout;
+}
+
 export class NodeLocalFileSystem extends AbstractLocalFileSystem {
   private rootDir: string;
 
