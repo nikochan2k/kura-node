3088b7404e2b76f47b8cc71550c6fe8ab3b33596
Yoshihiro Seki <nikochan2k@gmail.com>
2020-03-21 12:49:08 +0900

    update package

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,2 +1,15 @@
+const globalVar =
+  typeof window !== "undefined"
+    ? window
+    : typeof global !== "undefined"
+    ? global
+    : Function("return this;")();
+
+if (!globalVar.setTimeout || !globalVar.clearTimeout) {
+  const timers = require("timers");
+  globalVar.clearTimeout = timers.clearTimeout;
+  globalVar.setTimeout = timers.setTimeout;
+}
+
 export * from "./NodeLocalFileSystem";
 export * from "./NodeLocalFileSystemAsync";
