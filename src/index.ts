fdd86f953f4510cb6b30dad525c29624f5c4f98d
Yoshihiro Seki <nikochan2k@gmail.com>
2020-03-22 13:26:10 +0900

    export classes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-import * as ExpoFs from "./node";
-export { ExpoFs };
+const globalVar =
+  typeof window !== "undefined"
+    ? window
+    : typeof global !== "undefined"
+    ? global
+    : Function("return this;")();
+
+if (!globalVar.setTimeout || !globalVar.clearTimeout) {
+  const timers = require("timers");
+  globalVar.clearTimeout = timers.clearTimeout;
+  globalVar.setTimeout = timers.setTimeout;
+}
+
+export * from "./node";
