e3e92c2c1cc44de739fd5c42b67276f7fe1a77dd
Yoshihiro Seki <yoshihiro@YoshihironoMacBook-Pro.local>
2020-08-23 21:07:53 +0900

    fix export bug

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,2 @@
 import "tslib";
-
-const globalVar =
-  typeof window !== "undefined"
-    ? window
-    : typeof global !== "undefined"
-    ? global
-    : Function("return this;")();
-
-if (!globalVar.setTimeout || !globalVar.clearTimeout) {
-  const timers = require("timers");
-  globalVar.clearTimeout = timers.clearTimeout;
-  globalVar.setTimeout = timers.setTimeout;
-}
-
 export * from "./node";
--- src/node/NodeLocalFileSystem.ts
+++ src/node/NodeLocalFileSystem.ts
@@ -2,6 +2,19 @@ import { AbstractAccessor, AbstractLocalFileSystem } from "kura";
 import { FileSystemOptions } from "kura/lib/FileSystemOptions";
 import { NodeAccessor } from "./NodeAccessor";
 
+const globalVar =
+  typeof window !== "undefined"
+    ? window
+    : typeof global !== "undefined"
+    ? global
+    : Function("return this;")();
+
+if (!globalVar.setTimeout || !globalVar.clearTimeout) {
+  const timers = require("timers");
+  globalVar.clearTimeout = timers.clearTimeout;
+  globalVar.setTimeout = timers.setTimeout;
+}
+
 export class NodeLocalFileSystem extends AbstractLocalFileSystem {
   private rootDir: string;
 
