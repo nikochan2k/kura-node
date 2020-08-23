9201fcfea060628a02f48b2fab3ad3f7042eee27
Yoshihiro Seki <yoshihiro@YoshihironoMacBook-Pro.local>
2020-08-23 20:55:45 +0900

    import tslib

--- src/node/NodeLocalFileSystem.ts
+++ src/node/NodeLocalFileSystem.ts
@@ -2,15 +2,6 @@ import { AbstractAccessor, AbstractLocalFileSystem } from "kura";
 import { FileSystemOptions } from "kura/lib/FileSystemOptions";
 import { NodeAccessor } from "./NodeAccessor";
 
-if (
-  !(global && global.setTimeout && global.clearTimeout) &&
-  !(window && window.setTimeout && window.clearTimeout)
-) {
-  const timers = require("timers");
-  global.clearTimeout = timers.clearTimeout;
-  global.setTimeout = timers.setTimeout;
-}
-
 export class NodeLocalFileSystem extends AbstractLocalFileSystem {
   private rootDir: string;
 
