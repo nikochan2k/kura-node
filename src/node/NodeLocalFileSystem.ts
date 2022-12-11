a00b3faa813c3aef22370821c81ffb9986bcd247
Yoshihiro Seki <nikochan2k@gmail.com>
2022-12-11 10:13:50 +0900

    remove unused variables

--- src/node/NodeLocalFileSystem.ts
+++ src/node/NodeLocalFileSystem.ts
@@ -2,21 +2,6 @@ import { AbstractAccessor, AbstractLocalFileSystem } from "kura";
 import { FileSystemOptions } from "kura/lib/FileSystemOptions";
 import { NodeAccessor } from "./NodeAccessor";
 
-/* eslint-disable */
-const globalVar =
-  typeof window !== "undefined"
-    ? window
-    : typeof global !== "undefined"
-    ? global
-    : Function("return this;")();
-
-if (!globalVar.setTimeout || !globalVar.clearTimeout) {
-  const timers = require("timers");
-  globalVar.clearTimeout = timers.clearTimeout;
-  globalVar.setTimeout = timers.setTimeout;
-}
-/* eslint-enable */
-
 export class NodeLocalFileSystem extends AbstractLocalFileSystem {
   private rootDir: string;
 
